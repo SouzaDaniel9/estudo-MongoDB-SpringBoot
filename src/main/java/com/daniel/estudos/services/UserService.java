ebce59d0424286d0ae50fefc03bf7ac9a78cc095
Daniel Souza <danielflorindo9@gmail.com>
2023-11-28 16:05:19 -0300

    Obtendo um usuário por id

--- src/main/java/com/daniel/estudos/services/UserService.java
+++ src/main/java/com/daniel/estudos/services/UserService.java
@@ -1,21 +1,28 @@
 package com.daniel.estudos.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.daniel.estudos.domain.User;
+import com.daniel.estudos.exception.ObjectNotFoundException;
 import com.daniel.estudos.repository.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
 
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
