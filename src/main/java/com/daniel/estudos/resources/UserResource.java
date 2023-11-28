50ad89dd11e3a9ae935b22f161039b9cc31007a4
Daniel Souza <danielflorindo9@gmail.com>
2023-11-28 20:51:04 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/daniel/estudos/resources/UserResource.java
+++ src/main/java/com/daniel/estudos/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.daniel.estudos.domain.Post;
 import com.daniel.estudos.domain.User;
 import com.daniel.estudos.dto.UserDTO;
 import com.daniel.estudos.services.UserService;
@@ -62,4 +63,9 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@GetMapping("/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 }
